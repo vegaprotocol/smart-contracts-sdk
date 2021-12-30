3d65c20b9bd3105fc2b9faab9f0802be42bbbc4d
Matthew Russell <mattrussell36@gmail.com>
2021-12-30 12:57:59 -0800

    set exponential_at property of big number to prevent scientific notation

--- src/utils/decimals.ts
+++ src/utils/decimals.ts
@@ -1,5 +1,7 @@
 import BigNumber from 'bignumber.js';
 
+BigNumber.config({ EXPONENTIAL_AT: 20000 });
+
 export function addDecimal(value: BigNumber, decimals: number): BigNumber {
   return value.dividedBy(Math.pow(10, decimals)).decimalPlaces(decimals);
 }
