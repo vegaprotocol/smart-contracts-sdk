1b13adf4ea13bc2c9717f9711b3cd56c2314bff4
Matthew Russell <mattrussell36@gmail.com>
2022-01-07 13:58:11 -0800

    update vega token class to use base class

--- src/contracts/vega-token.ts
+++ src/contracts/vega-token.ts
@@ -1,61 +1,59 @@
 import BigNumber from 'bignumber.js';
-import { ethers } from 'ethers';
+import { ethers, BigNumber as EthersBigNumber } from 'ethers';
+import { EnvironmentConfig, Networks } from '..';
 
 import tokenAbi from '../abis/vega_token_abi.json';
-import { addDecimal, removeDecimal } from '../utils/decimals';
+import { BaseContract } from './base-contract';
 
-export class VegaToken {
-  private contract: ethers.Contract;
-  public dp: Promise<number>;
+export class VegaToken extends BaseContract {
+  public contract: ethers.Contract;
 
-  constructor(
-    provider: ethers.providers.BaseProvider,
-    signer: ethers.Signer | null,
-    tokenAddress: string
-  ) {
-    const self = this;
-
+  constructor(provider: ethers.providers.Web3Provider, network: Networks) {
+    super(provider, network);
     this.contract = new ethers.Contract(
-      tokenAddress,
+      EnvironmentConfig[network].vegaTokenAddress,
       tokenAbi,
-      signer || provider
+      this.signer || this.provider
     );
-
-    this.dp = (async () => {
-      const val = await self.contract.decimals();
-      return Number(val);
-    })();
   }
 
+  /** Gets Ethereum account's permitted allowance */
   async allowance(address: string, spender: string): Promise<BigNumber> {
-    const decimals = await this.dp;
-    const res = await this.contract.allowance(address, spender);
-    return addDecimal(new BigNumber(res.toString()), decimals);
-  }
-
-  async approve(spender: string): Promise<ethers.ContractTransaction> {
-    const decimals = await this.dp;
-    const amount = removeDecimal(
-      new BigNumber(Number.MAX_SAFE_INTEGER - 1),
-      decimals
+    const res: EthersBigNumber = await this.contract.allowance(
+      address,
+      spender
     );
-    return this.contract.approve(spender, amount.toString());
+    const value = await this.addDecimal(res);
+    return value;
   }
 
+  /** Executs contracts approve function */
+  async approve(spender: string): Promise<ethers.ContractTransaction> {
+    const amount = this.removeDecimal(new BigNumber(Number.MAX_SAFE_INTEGER));
+    const tx = await this.contract.approve(spender, amount);
+
+    this.trackTransaction(tx, 3);
+
+    return tx;
+  }
+
+  /** Gets Vega token total supply */
   async totalSupply(): Promise<BigNumber> {
-    const decimals = await this.dp;
-    const res: BigNumber = await this.contract.totalSupply();
-    return addDecimal(new BigNumber(res.toString()), decimals);
+    const res: EthersBigNumber = await this.contract.totalSupply();
+    const value = await this.addDecimal(res);
+    return value;
   }
 
+  /** Gets number of decimals used by token */
   async decimals(): Promise<number> {
     const res: number = await this.contract.decimals();
     return Number(res);
   }
 
+  /** Gets number tokens an Ethereum account owns */
   async balanceOf(address: string): Promise<BigNumber> {
-    const decimals = await this.dp;
-    const res: BigNumber = await this.contract.balanceOf(address);
-    return addDecimal(new BigNumber(res.toString()), decimals);
+    const res: EthersBigNumber = await this.contract.balanceOf(address);
+    const value = await this.addDecimal(res);
+    return value;
   }
 }
