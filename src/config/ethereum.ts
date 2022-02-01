9ac26798ff1849f5b1480e2e0421bd73d0b558fa
Dexter <dexter.edwards93@gmail.com>
2022-02-01 09:17:55 +0000

    update contract addresses
    https://github.com/vegaprotocol/devops-infra/pull/1130/files#diff-7e3f704a74875eb55f3afd9ecb3a9eb9894751bc772a28ddc73043ebbf42c1adR130

--- src/config/ethereum.ts
+++ src/config/ethereum.ts
@@ -61,39 +61,39 @@ export const EnvironmentConfig: { [key in Networks]: VegaContracts } = {
     vegaTokenAddress: '0xc93137f9F4B820Ca85FfA3C7e84cCa6Ebc7bB517',
     claimAddress: '0x8Cef746ab7C83B61F6461cC92882bD61AB65a994',
     lockedAddress: '0x0',
-    vestingAddress: '0xd751FF6264234cAfAE88e4BF6003878fAB9630a7',
-    stakingBridge: '0x3cCe40e1e47cedf76c03db3E48507f421b575523',
-    erc20Bridge: '0x042573A44C7ed0c03960ce505Bd60C6d90d23795',
+    vestingAddress: '0xd1216AAb948f5FC706Df73df6d71c64CcaA8550a',
+    stakingBridge: '0xf2cD5C8b8c52f96293338A0AF463a0Bfc602D5bc',
+    erc20Bridge: '0xE43013C3c2A134AB3782ADEb258669A8566DAD57',
   },
   [Networks.STAGNET]: {
-    vestingAddress: '0xfCe6eB272D3d4146A96bC28de71212b327F575fa',
     vegaTokenAddress: '0x547cbA83a7eb82b546ee5C7ff0527F258Ba4546D',
     claimAddress: '0x8Cef746ab7C83B61F6461cC92882bD61AB65a994', // TODO not deployed to this env, but random address so app doesn't error
     lockedAddress: '0x0', // TODO not deployed to this env
+    vestingAddress: '0xfCe6eB272D3d4146A96bC28de71212b327F575fa',
     stakingBridge: '0x7D88CD817227D599815d407D929af18Bb8D57176',
     erc20Bridge: '0xc0835e6dEf177F8ba2561C4e4216827A3798c6B9',
   },
   [Networks.STAGNET2]: {
-    vestingAddress: '0x005F13184cf57B9EE49701c8D8c952534c36AcaB',
     vegaTokenAddress: '0xd8fa193B93a179DdCf51FFFDe5320E0872cdcf44',
     claimAddress: '0x8Cef746ab7C83B61F6461cC92882bD61AB65a994', // TODO not deployed to this env, but random address so app doesn't error
     lockedAddress: '0x0', // TODO not deployed to this env
-    stakingBridge: '0xCee201E44ADe5400Ceb0d5924e0802244B6c40f7',
-    erc20Bridge: '0xEbabe46685157A43578DD63Edb0430ef48B5a5b0',
+    vestingAddress: '0x9F10cBeEf03A564Fb914c2010c0Cd55E9BB11406',
+    stakingBridge: '0x7896C9491962D5839783CB6e0492ECebd34Bb35F',
+    erc20Bridge: '0xffC1eb64e22fd5E29816c633eE84088EEEe879E5',
   },
   [Networks.TESTNET]: {
-    vestingAddress: '0xe2deBB240b43EDfEBc9c38B67c0894B9A92Bf07c',
     vegaTokenAddress: '0xDc335304979D378255015c33AbFf09B60c31EBAb',
     claimAddress: '0x8Cef746ab7C83B61F6461cC92882bD61AB65a994', // TODO not deployed to this env, but random address so app doesn't error
     lockedAddress: '0x0', // TODO not deployed to this env
+    vestingAddress: '0xe2deBB240b43EDfEBc9c38B67c0894B9A92Bf07c',
     stakingBridge: '0xF5A3830F002BE78dd801214F5316b677E0355c60',
     erc20Bridge: '0xF009C66c6afC9661143fD7cE1eDb02c1961a6510',
   },
   [Networks.MAINNET]: {
-    vestingAddress: '0x23d1bFE8fA50a167816fBD79D7932577c06011f4',
     vegaTokenAddress: '0xcB84d72e61e383767C4DFEb2d8ff7f4FB89abc6e',
     claimAddress: '0x0ee1fb382caf98e86e97e51f9f42f8b4654020f3',
     lockedAddress: '0x78344c7305d73a7a0ac3c94cd9960f4449a1814e',
+    vestingAddress: '0x23d1bFE8fA50a167816fBD79D7932577c06011f4',
     stakingBridge: '0x195064D33f09e0c42cF98E665D9506e0dC17de68',
     erc20Bridge: '0xCd403f722b76366f7d609842C589906ca051310f',
   },
